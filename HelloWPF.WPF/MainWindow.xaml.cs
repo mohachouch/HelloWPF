e4feba44551df6cf93affaf1dd925bee023f91a2
Mohamed CHOUCHANE <mohamed.chouchane1990@gmail.com>
2017-12-28 20:47:26 +0100

    Ajoutez des fichiers projet.

---
+++ HelloWPF.WPF/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms.Platform.WPF;
+
+namespace HelloWPF.WPF
+{
+	/// <summary>
+	/// Logique d'interaction pour MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : FormsApplicationPage
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			Xamarin.Forms.Forms.Init();
+			LoadApplication(new HelloWPF.App());
+		}
+	}
+}
